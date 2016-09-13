896069f31d90adf35393320506d323368189ee87
susnm <wwzmzs@gmail.com>
2016-09-14 01:08:02 +0800

    Revert "init"
    This reverts commit 762a512b963c5823794b6e94e50037630e4bb9cc.

--- 下载进度绘制/ViewController.swift
+++ 下载进度绘制/ViewController.swift
@@ -10,26 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBOutlet var progressView: ProgressView!
-  @IBOutlet var sliderView: UISlider!
-  
-  @IBAction func slideValueChange(_ sender: UISlider) {
-    progressView.progress = CGFloat(sender.value)
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    let proView = ProgressView(frame: CGRect(x: 20, y: 20, width: 200, height: 100))
-    view.addSubview(proView)
-    
+    // Do any additional setup after loading the view, typically from a nib.
   }
 
-  @IBAction func changeProgress(_ sender: UIButton) {
-    
-    sliderView.setValue(0.4, animated: true)
-    progressView.setProgress(progress: 0.4, withAnimated: true)
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
   }
+
 
 }
 
